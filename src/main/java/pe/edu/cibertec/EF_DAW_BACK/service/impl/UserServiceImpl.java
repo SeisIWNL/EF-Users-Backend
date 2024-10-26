676138aa15b408cae0854feea48b3e863946bc64
Seis <ivanrup123@gmail.com>
2024-10-25 20:34:06 -0500

    UserServiceImpl update

--- src/main/java/pe/edu/cibertec/EF_DAW_BACK/service/impl/UserServiceImpl.java
+++ src/main/java/pe/edu/cibertec/EF_DAW_BACK/service/impl/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package pe.edu.cibertec.EF_DAW_BACK.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
@@ -9,8 +11,7 @@ import pe.edu.cibertec.EF_DAW_BACK.service.UserService;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,6 +22,9 @@ public class UserServiceImpl implements UserService {
     private ResourceLoader resourceLoader;
 
     private Resource resource;
+
+    //Instanciando la variable logger para depuración de errores
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Override
     public String[] validarUser(LoginRequestDTO loginRequestDTO) throws IOException {
@@ -53,14 +57,20 @@ public class UserServiceImpl implements UserService {
 
         List<String[]> usuarios = new ArrayList<>();
 
-        try (BufferedReader br = Files.newBufferedReader(Path.of(resource.getFile().toURI()))) {
+        //Metodo que lee los inputs del archivo independientemente en que lugar se encuentren
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(resource.getInputStream()))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] data = line.split(";");
+                if (data.length < 7) {
+                    logger.warn("Invalid line in integrantes.txt: " + line);
+                    continue;
+                }
                 usuarios.add(data);
             }
         } catch (IOException e) {
-            throw new IOException("Error al leer el archivo de integrantes.txt", e);
+            logger.error("Error reading integrantes.txt", e);
+            throw new IOException("Error reading integrantes.txt", e);
         }
         return usuarios;
     }
